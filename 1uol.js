3ad51b94e2fce0ab2074e44ce746c1f80df12a04
Akinbobola1 <92653086+Akinbobola1@users.noreply.github.com>
2022-05-06 22:00:21 +0100

    Create 1uol.js

---
+++ 1uol.js
@@ -0,0 +1,44 @@
+ <script type="text/javascript" src="../../ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+  <script type="text/javascript">
+< !--//-->
+  < ![CDATA[//><!--
+        window.jQuery || document.write("<script src='../sites/all/modules/jquery_update/replace/jquery/3.1/jquery.js'>\x3C/script>")
+//--><!]]>
+  </script>
+  <script type="text/javascript">
+  < !--//-->
+  < ![CDATA[//><!--
+    jQuery.migrateMute = true; jQuery.migrateTrace = false;
+//--><!]]>
+  </script>
+  <script type="text/javascript" src="../../code.jquery.com/jquery-migrate-3.0.0.js"></script>
+  <script type="text/javascript">
+< !--//-->
+  < ![CDATA[//><!--
+      window.jQuery && window.jQuery.migrateWarnings || document.write("<script src='../sites/all/modules/jquery_update/replace/jquery-migrate/3/jquery-migrate.js'>\x3C/script>")
+//--><!]]>
+  </script>
+  <script type="text/javascript"
+    src="../sites/default/files/js/js_38VWQ3jjQx0wRFj7gkntZr077GgJoGn5nv3v05IeLLo.js"></script>
+  <script type="text/javascript"
+    src="../sites/default/files/js/js_6WjiFgvmHfmfHrKGHz-A0lTB6gHYOj0W5sdi01IZZqI.js"></script>
+  <script type="text/javascript"
+    src="../sites/default/files/js/js_c9S7L28GiD9hlQGPOAZ-TmAjXzauQOFTS3F37BGHtCQ.js"></script>
+  <script type="text/javascript"
+    src="../sites/default/files/js/js_ZrrsDterNYfdOzj3jevTAURVWDcaO592zXxIPu4nPCM.js"></script>
+  <script type="text/javascript">
+  < !--//-->
+  < ![CDATA[//><!--
+    (function (i, s, o, g, r, a, m) { i["GoogleAnalyticsObject"] = r; i[r] = i[r] || function () { (i[r].q = i[r].q || []).push(arguments) }, i[r].l = 1 * new Date(); a = s.createElement(o), m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m) })(window, document, "script", "../../www.google-analytics.com/analytics.js", "ga"); ga("create", "UA-40945919-1", { "cookieDomain": "auto" }); ga("set", "anonymizeIp", true); ga("send", "pageview");
+//--><!]]>
+  </script>
+  <script type="text/javascript"
+    src="../sites/default/files/js/js_x35gejs7okfWzFxyoXwnfjvMoeBDf7XDlViZxxNL1nw.js"></script>
+  <script type="text/javascript"
+    src="../sites/default/files/js/js_4HeFKK-wzaOXqg0Kdr3Ftx7aW2HqAV6hdq_PFGXKRf8.js"></script>
+  <script type="text/javascript">
+< !--//-->
+  < ![CDATA[//><!--
+                jQuery.extend(Drupal.settings, { "basePath": "\/", "pathPrefix": "", "ajaxPageState": { "theme": "basic", "theme_token": "Wa9nWRBjCEB2t8h7K5UbjA7NHoHA14Bazm_bK1GJ7uA", "jquery_version": "3.1", "js": { "\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/3.1.1\/jquery.js": 1, "0": 1, "1": 1, "\/\/code.jquery.com\/jquery-migrate-3.0.0.js": 1, "2": 1, "misc\/jquery-extend-3.4.0.js": 1, "misc\/jquery-html-prefilter-3.5.0-backport.js": 1, "misc\/jquery.once.js": 1, "misc\/drupal.js": 1, "misc\/ajax.js": 1, "sites\/all\/modules\/jquery_update\/js\/jquery_update.js": 1, "sites\/all\/modules\/simpleads\/simpleads.js": 1, "sites\/all\/modules\/simpleads\/modules\/simpleads_campaigns\/simpleads_campaigns.js": 1, "sites\/all\/libraries\/colorbox\/jquery.colorbox.js": 1, "sites\/all\/modules\/colorbox\/js\/colorbox.js": 1, "sites\/all\/modules\/colorbox\/styles\/default\/colorbox_style.js": 1, "sites\/all\/modules\/colorbox\/js\/colorbox_load.js": 1, "sites\/all\/modules\/colorbox\/js\/colorbox_inline.js": 1, "sites\/all\/modules\/google_analytics\/googleanalytics.js": 1, "3": 1, "misc\/progress.js": 1, "sites\/all\/themes\/basic\/js\/build\/selectivizr-min.js": 1, "sites\/all\/modules\/colorbox_node\/colorbox_node.js": 1, "sites\/all\/themes\/basic\/js\/build\/scripts.js": 1, "sites\/all\/themes\/basic\/js\/build\/modernizr.js": 1 }, "css": { "modules\/system\/system.base.css": 1, "modules\/system\/system.menus.css": 1, "modules\/system\/system.messages.css": 1, "modules\/system\/system.theme.css": 1, "sites\/all\/modules\/colorbox_node\/colorbox_node.css": 1, "modules\/comment\/comment.css": 1, "modules\/field\/theme\/field.css": 1, "modules\/node\/node.css": 1, "sites\/all\/modules\/simpleads\/simpleads.css": 1, "modules\/user\/user.css": 1, "sites\/all\/modules\/views\/css\/views.css": 1, "sites\/all\/modules\/colorbox\/styles\/default\/colorbox_style.css": 1, "sites\/all\/modules\/ctools\/css\/ctools.css": 1, "sites\/all\/modules\/panels\/css\/panels.css": 1, "sites\/all\/themes\/basic\/css\/default.css": 1, "sites\/all\/themes\/basic\/css\/layout.css": 1, "sites\/all\/themes\/basic\/css\/style.css": 1, "sites\/all\/themes\/basic\/css\/normalize.css": 1, "sites\/all\/themes\/basic\/css\/print.css": 1, "sites\/all\/themes\/basic\/css\/ie7.css": 1, "sites\/all\/themes\/basic\/css\/ie8.css": 1 } }, "colorbox": { "opacity": "0.85", "current": "{current} of {total}", "previous": "\u00ab Prev", "next": "Next \u00bb", "close": "Close", "maxWidth": "98%", "maxHeight": "98%", "fixed": true, "mobiledetect": false, "mobiledevicewidth": "480px", "specificPagesDefaultValue": "admin*\nimagebrowser*\nimg_assist*\nimce*\nnode\/add\/*\nnode\/*\/edit\nprint\/*\nprintpdf\/*\nsystem\/ajax\nsystem\/ajax\/*" }, "better_exposed_filters": { "views": { "field_collection_list": { "displays": { "block_2": { "filters": [] }, "block_1": { "filters": [] } } } } }, "googleanalytics": { "trackOutbound": 1, "trackMailto": 1, "trackDownload": 1, "trackDownloadExtensions": "7z|aac|arc|arj|asf|asx|avi|bin|csv|doc(x|m)?|dot(x|m)?|exe|flv|gif|gz|gzip|hqx|jar|jpe?g|js|mp(2|3|4|e?g)|mov(ie)?|msi|msp|pdf|phps|png|ppt(x|m)?|pot(x|m)?|pps(x|m)?|ppam|sld(x|m)?|thmx|qtm?|ra(m|r)?|sea|sit|tar|tgz|torrent|txt|wav|wma|wmv|wpd|xls(x|m|b)?|xlt(x|m)|xlam|xml|z|zip", "trackColorbox": 1 }, "colorbox_node": { "width": "600px", "height": "600px" } });
+//--><!]]>
+  </script>
